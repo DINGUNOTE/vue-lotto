230dc540419897e06b2643c43a58ce7ba451f19c
이진규 <web.ljg@gmail.com>
2024-11-15 16:32:43 +0900

    Updates

--- assets/GetNumbersView-JIFANhQw.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,c as s,a as o,o as c}from"./index-Cq7bui9w.js";const r={},a={class:"about"};function n(_,e){return c(),s("div",a,e[0]||(e[0]=[o("h1",{class:"text-center"},"서비스 준비중입니다.",-1)]))}const f=t(r,[["render",n]]);export{f as default};
