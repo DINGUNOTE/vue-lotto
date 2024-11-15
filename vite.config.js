072463e61cdaef940b23624c27060a0008c02193
이진규 <web.ljg@gmail.com>
2024-11-15 16:21:09 +0900

    gh-pages 추가 및 배포 준비

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
+  base: '/vue-lotto/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
