c097ce41c14c27e1a8220906f48f5b4946d1e088
이진규 <web.ljg@gmail.com>
2024-11-25 15:10:19 +0900

    번호 뽑기 페이지 작업중

--- src/views/GetNumbersView.vue
+++ src/views/GetNumbersView.vue
@@ -1,22 +1,60 @@
-<script setup lang="ts"></script>
+<script setup></script>
 
 <template>
   <section>
-    <div>
+    <article>
       <h1>
         고정 번호
 
-        <v-icon size="24">mdi-help</v-icon>
-        <v-tooltip activator="parent" location="end"
-          >해당 번호는 고정으로 포함시켜서 번호를 추첨합니다.</v-tooltip
-        >
+        <div class="tooltip-box">
+          <v-icon size="24"> mdi-help-circle-outline </v-icon>
+          <v-tooltip activator="parent" location="top"
+            >해당 번호를 고정으로 포함시켜서 번호를 추첨합니다.<br />최소 1개부터 최대 5개의 번호를
+            선택 가능합니다.</v-tooltip
+          >
+        </div>
       </h1>
-    </div>
 
-    <div>
-      <h1>제외 번호</h1>
-    </div>
+      <v-text-field
+        variant="underlined"
+        type="number"
+        placeholder="고정 번호 입력"
+        clearable
+      ></v-text-field>
+
+      <v-card>
+        <ul>
+          <li></li>
+        </ul>
+      </v-card>
+    </article>
+
+    <article>
+      <h1>
+        제외 번호
+
+        <div class="tooltip-box">
+          <v-icon size="24"> mdi-help-circle-outline </v-icon>
+          <v-tooltip activator="parent" location="top"
+            >해당 번호를 제외하고 번호를 추첨합니다.<br />최소 0개부터 최대 39개까지 선택
+            가능합니다.</v-tooltip
+          >
+        </div>
+      </h1>
+    </article>
   </section>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+section {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+}
+
+h1 {
+  display: flex;
+  align-items: center;
+  gap: 0.125rem;
+}
+</style>
