098406f67d22f6d86b87eb89b9dc54b92c7e9973
이진규 <web.ljg@gmail.com>
2024-11-25 11:18:49 +0900

    chore: mdi/js 삭제

--- src/views/GetNumbersView.vue
+++ src/views/GetNumbersView.vue
@@ -1,7 +1,22 @@
+<script setup lang="ts"></script>
+
 <template>
-  <div class="about">
-    <h1 class="text-center">서비스 준비중입니다.</h1>
-  </div>
+  <section>
+    <div>
+      <h1>
+        고정 번호
+
+        <v-icon size="24">mdi-help</v-icon>
+        <v-tooltip activator="parent" location="end"
+          >해당 번호는 고정으로 포함시켜서 번호를 추첨합니다.</v-tooltip
+        >
+      </h1>
+    </div>
+
+    <div>
+      <h1>제외 번호</h1>
+    </div>
+  </section>
 </template>
 
 <style scoped></style>
